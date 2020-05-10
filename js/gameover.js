40b6f4db7d23392b13004626431f4ab1bd367d73
StepIgor <igor.stepanov@difres.ru>
2020-05-10 20:16:54 +0400

    остерегайтесь неявного преобразования!

--- js/gameover.js
+++ js/gameover.js
@@ -4,7 +4,7 @@ if (sessionStorage.getItem('last_result') == null){ sessionStorage.setItem('last
 
 $(document).ready(function(){
   show_result = setInterval(function(){
-    if (shows_now != sessionStorage.getItem('last_result')){
+    if (shows_now != parseInt(sessionStorage.getItem('last_result'))){
       shows_now++;
       $('#last_result').text(shows_now);
     } else {
@@ -15,39 +15,27 @@ $(document).ready(function(){
   $('#my_record').text(localStorage.getItem('max_score'));
   $('#previous_result').text(JSON.parse(localStorage.getItem('last_scores'))[4]);
 
-  if (sessionStorage.getItem('last_result') > localStorage.getItem('max_score')){
-    $('#record_diff').text('+' + (sessionStorage.getItem('last_result') - localStorage.getItem('max_score')));
+  if (parseInt(sessionStorage.getItem('last_result')) > parseInt(localStorage.getItem('max_score'))){
+    $('#record_diff').text('+' + (parseInt(sessionStorage.getItem('last_result')) - parseInt(localStorage.getItem('max_score'))));
     document.getElementById('record_diff').classList.add('green-text');
-  } else if (sessionStorage.getItem('last_result') == localStorage.getItem('max_score')){
+  } else if (parseInt(sessionStorage.getItem('last_result')) == parseInt(localStorage.getItem('max_score'))){
     $('#record_diff').text(0);
   } else {
-    $('#record_diff').text(sessionStorage.getItem('last_result') - localStorage.getItem('max_score'));
+    $('#record_diff').text(parseInt(sessionStorage.getItem('last_result')) - parseInt(localStorage.getItem('max_score')));
     document.getElementById('record_diff').classList.add('red-text');
   }
 
-  if (sessionStorage.getItem('last_result') > JSON.parse(localStorage.getItem('last_scores'))[4]){
-    $('#previous_diff').text('+' + (sessionStorage.getItem('last_result') - JSON.parse(localStorage.getItem('last_scores'))[4]));
+  if (parseInt(sessionStorage.getItem('last_result')) > parseInt(JSON.parse(localStorage.getItem('last_scores'))[4])){
+    $('#previous_diff').text('+' + (parseInt(sessionStorage.getItem('last_result')) - parseInt(JSON.parse(localStorage.getItem('last_scores'))[4])));
     document.getElementById('previous_diff').classList.add('green-text');
-  } else if (sessionStorage.getItem('last_result') == JSON.parse(localStorage.getItem('last_scores'))[4]){
+  } else if (parseInt(sessionStorage.getItem('last_result')) == parseInt(JSON.parse(localStorage.getItem('last_scores'))[4])){
     $('#previous_diff').text(0);
   } else {
-    $('#previous_diff').text(sessionStorage.getItem('last_result') - JSON.parse(localStorage.getItem('last_scores'))[4]);
+    $('#previous_diff').text(parseInt(sessionStorage.getItem('last_result')) - parseInt(JSON.parse(localStorage.getItem('last_scores'))[4]));
     document.getElementById('previous_diff').classList.add('red-text');
   }
 
-  if (sessionStorage.getItem('last_result') > localStorage.getItem('max_score')){
-    localStorage.setItem('max_score', sessionStorage.getItem('last_result'));
-    $('#new_record').css({'display':'block'});
-  }
-
-  var lst_scores = JSON.parse(localStorage.getItem('last_scores'));
-
-  for (k = 0; k<= 3; k++){
-    lst_scores[k] = lst_scores[k+1];
-  }
-  lst_scores[4] = parseInt(sessionStorage.getItem('last_result'));
-
-  localStorage.setItem('last_scores', JSON.stringify(lst_scores));
+  setTimeout(can_change_record, 500);
 
 });
 
@@ -59,3 +47,19 @@ document.getElementById('to_menu_but').addEventListener('click',function(){
   audio.play();
   setTimeout(function(){document.location.href = 'index.html'}, 2000);
 });
+
+function can_change_record(){
+  if (parseInt(sessionStorage.getItem('last_result')) > parseInt(localStorage.getItem('max_score'))){
+    localStorage.setItem('max_score', parseInt(sessionStorage.getItem('last_result')));
+    $('#new_record').css({'display':'block'});
+  }
+
+  var lst_scores = JSON.parse(localStorage.getItem('last_scores'));
+
+  for (k = 0; k<= 3; k++){
+    lst_scores[k] = lst_scores[k+1];
+  }
+  lst_scores[4] = parseInt(sessionStorage.getItem('last_result'));
+
+  localStorage.setItem('last_scores', JSON.stringify(lst_scores));
+}
